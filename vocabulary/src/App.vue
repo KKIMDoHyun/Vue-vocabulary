6c8d33ede6e2f47707a9761ac6d0343e57751be2
KKIMDoHyun <kdh5998@naver.com>
2021-08-23 17:47:18 +0900

    프로젝트 생성

---
+++ vocabulary/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <VocabularyHeader></VocabularyHeader>
+    <VocabularyInput></VocabularyInput>
+    <VocabularyFooter></VocabularyFooter>
+  </div>
+</template>
+
+<script>
+import VocabularyHeader from './components/VocabularyHeader.vue'
+import VocabularyFooter from './components/VocabularyFooter.vue'
+import VocabularyInput from './components/VocabularyInput.vue'
+export default {
+  name: 'App',
+  components: {
+    VocabularyHeader,
+    VocabularyInput,
+    VocabularyFooter
+  }
+}
+</script>
+
+<style>
+body {
+  text-align: center;
+  background-color: #F6F6F6;
+}
+</style>
---
+++ vocabulary/src/components/VocabularyInput.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+      <input type="text" v-model="word">
+  </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            word: '',
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
